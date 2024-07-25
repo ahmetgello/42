45450a38385f1e7c049e3d6ce746d53a9064107a
Ahmet Kallo <akallo@k1m15s01.42istanbul.com.tr>
2024-07-25 11:22:47 +0300

    updated C00. C00 ex00 to ex07 OK, no ex08

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: akallo <akallo@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 13:46:57 by akallo            #+#    #+#             */
-/*   Updated: 2024/07/09 13:52:11 by akallo           ###   ########.fr       */
+/*   Updated: 2024/07/09 13:54:01 by akallo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ void	ft_putchar(char c)
 	write (1, &c, 1);
 }
 
-void	ft_write_comb(int a, int b, int last)
+void	ft_write_comb(int first_part, int second_part, int last)
 {
-	ft_putchar(48 + a / 10);
-	ft_putchar(48 + a % 10);
+	ft_putchar(48 + first_part / 10);
+	ft_putchar(48 + first_part % 10);
 	ft_putchar(' ');
-	ft_putchar(48 + b / 10);
-	ft_putchar(48 + b % 10);
+	ft_putchar(48 + second_part / 10);
+	ft_putchar(48 + second_part % 10);
 	if (last == 1)
 	{
 		ft_putchar(',');
@@ -33,20 +33,20 @@ void	ft_write_comb(int a, int b, int last)
 
 void	ft_print_comb2(void)
 {
-	int	a;
-	int	b;
+	int	first_part;
+	int	second_part;
 	int	last;
 
-	a = 0;
-	while (a <= 99)
+	first_part = 0;
+	while (first_part <= 99)
 	{
-		b = a + 1;
-		while (b <= 99)
+		second_part = first_part + 1;
+		while (second_part <= 99)
 		{
-			last = !(a == 98 && b == 99);
-			ft_write_comb(a, b, last);
-			b++;
+			last = !(first_part == 98 && second_part == 99);
+			ft_write_comb(first_part, second_part, last);
+			second_part++;
 		}
-		a++;
+		first_part++;
 	}
 }
---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akallo <akallo@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/09 14:00:13 by akallo            #+#    #+#             */
+/*   Updated: 2024/07/09 14:05:48 by akallo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+		ft_putnbr(nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
+}
